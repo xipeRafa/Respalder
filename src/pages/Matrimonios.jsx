bb5a330fd0cef31cfd9fa6e3250980275c04af6c
fer <fer@192.168.2.7>
2024-08-12 00:13:30 -0700

    lplp

--- src/pages/Matrimonios.jsx
+++ src/pages/Matrimonios.jsx
@@ -50,7 +50,8 @@ export default function Matrimonios({ arrOrders, setGetArrOrders, getArrOrders }
                                     
                                 <div className='formInfoToSave'>
                                     <p>Respaldar Acta de Matrimonio</p>
-                                    <input type="text" placeholder='Nombre' />
+                                    <input type="text" placeholder='Nombre del Esposo' />
+                                    <input type="text" placeholder='Nombre de la Esposa' />
 
                                     {/*<label for="avatar">Choose a profile picture:</label>*/}
                                     <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" />
@@ -63,12 +64,16 @@ export default function Matrimonios({ arrOrders, setGetArrOrders, getArrOrders }
 
                             <div className='formInfoToFind'>
                                 <div>
-                                    <label htmlFor="avatar1">Buscar con Nombre:</label>
-                                    <input type="text" id='avatar1' placeholder='Nombre' />
+                                    <label htmlFor="avatar1">Buscar con Nombre del Esposo:</label>
+                                    <input type="text" id='avatar1' placeholder='Nombre...' />
+                                </div>
+                                <div>
+                                    <label htmlFor="avatar1">Buscar con Nombre de la Esposa:</label>
+                                    <input type="text" id='avatar1' placeholder='Nombre...' />
                                 </div>
                                 <div>
                                     <label htmlFor="avatar2">Buscar con Fecha:</label>
-                                    <input type="date" id='avatar2' placeholder='Nombre' />
+                                    <input type="date" id='avatar2'  />
                                 </div>
                             </div>
                 }
