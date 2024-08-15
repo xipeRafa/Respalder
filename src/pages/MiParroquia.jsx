505c300bd49a891a9b00e18dedede1dfe5a1dcc3
MBAir 2017 <mbair2017@192.168.2.6>
2024-08-14 22:14:50 -0700

    kkkk

--- src/pages/MiParroquia.jsx
+++ src/pages/MiParroquia.jsx
@@ -28,6 +28,44 @@ export default function MiParroquia() {
 
 
 
+const[objectState, setObjectState]=useState({
+    name:'', email:'', password:'', password2:''
+})
+console.log(objectState)
+
+const {name, email, password, password2} = objectState
+
+  const handlerObjectsState=(e)=>{
+        const {name, value}=e.target
+        setObjectState({...objectState, [name]:value})
+  }
+
+  const [error, setError] = useState('');
+
+
+console.log(error)
+
+
+
+  
+  const enviar = (e) => {
+
+
+      setError('');
+
+      if (password !== password2) {
+          return setError("Claves No son iguales.");
+      }
+
+      if (password.length < 6) {
+          return setError("Claves es muy corta Use 6 caracteres Minimo");
+      }
+
+      register(authApp, email, password);
+
+
+
+}
 
 
 
@@ -49,11 +87,11 @@ export default function MiParroquia() {
 
                     {stateButtons ?
                         <div className={style.inputBG}>
-                            <div><input type="text" name=''  onChange={(e)=>handlerObjectsState(e)} placeholder='Nombre'/></div> 
-                            <div><input type="email" name='' onChange={(e)=>handlerObjectsState(e)} placeholder='Correo'/></div>
-                            <div><input type="text" name=''  onChange={(e)=>handlerObjectsState(e)} placeholder='Password'/></div> 
-                            <div><input type="text" name=''  onChange={(e)=>handlerObjectsState(e)} placeholder='Confirmar Password'/></div>
-                            <div><button>ENVIAR</button></div>
+                            <div><input type="text" name='name'  onChange={(e)=>handlerObjectsState(e)} placeholder='Nombre'/></div> 
+                            <div><input type="email" name='email' onChange={(e)=>handlerObjectsState(e)} placeholder='Correo'/></div>
+                            <div><input type="text" name='password'  onChange={(e)=>handlerObjectsState(e)} placeholder='Password'/></div> 
+                            <div><input type="text" name='password2'  onChange={(e)=>handlerObjectsState(e)} placeholder='Confirmar Password'/></div>
+                            <div><button onClick={()=>enviar()} >ENVIAR</button></div>
                         </div>
 
                         :
